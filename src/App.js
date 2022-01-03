79ff2dff4a467fff92ae20255b85d18ba82ca874
hyeoz <jenny970203@gmail.com>
2022-01-03 18:36:13 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import MyComponent from './MyComponent';
 
-function App() {
+const App = () => {
+  const name = '리액트';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="react">{name}</h1>
+      <MyComponent name={name} favNum={333}>
+        REACT
+      </MyComponent>
+      {/* 컴포넌트가 변수를 받는다면 사용할 때 변수를 넘겨주어야 함 */}
+      {/* 열고 닫는 태그 사이에 들어가는 값이 children 값이 됨 */}
     </div>
   );
-}
+};
 
 export default App;
